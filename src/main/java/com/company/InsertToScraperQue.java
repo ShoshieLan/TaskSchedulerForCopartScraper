1d8e1e8ba732d2de900847aff6f9d77a5b85e03b
Shoshie Lan <shoshielan@gmail.com>
2017-12-05 10:09:53 -0500

    i dont remember again

--- src/main/java/com/company/InsertToScraperQue.java
+++ src/main/java/com/company/InsertToScraperQue.java
@@ -27,25 +27,12 @@ public class InsertToScraperQue implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
-        //ResultSet rs = sqlGetResult("Select * from ArrayListBackupForCopartNotes");
+
         System.out.println("thirty min ago " + getCurrentDateMinus30Min());
 
-        if (list == null) {
-            ResultSet rs3 = sqlGetResult("Select * from ArrayListBackupForCopartNotes");
-            try {
-                while (rs3.next()) {
-
-                    list.add(String.valueOf(rs3));
-                    System.out.println(list);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            System.out.println(list);
-
-
-            if (list != null) {
-                list.clear();
+        if (list != null) {
+            list.clear();
+        }
                 ResultSet rs = sqlGetResult("Select * from ArrayListBackupForCopartNotes");
                 try {
                     while (rs.next()) {
@@ -65,10 +52,11 @@ public class InsertToScraperQue implements Job {
                     publishToQueue("celery", element + "," + getCurrentDateMinus30Min());
                     System.out.println(element + " " + getCurrentDateMinus30Min());
                     //System.out.println("you made it here");
-
+                    sqlQueryUpdate("Truncate table ArrayListBackupForCopartNotes");
                 }
             }
+
         }
-        sqlQueryUpdate("Truncate table ArrayListBackupForCopartNotes");
-    }
-}
+
+
+
